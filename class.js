5c1b00835c11932a902567d78f5a26577d97d0c3
Bill <billmike1994@gmail.com>
2017-08-30 20:05:59 +0100

    Refactored Function

--- class.js
+++ class.js
@@ -1,18 +1,57 @@
 // Simple Java OOP program that creates a simple Bio of a user
-function PersonalData(firstName, lastName, age, gender, personalInterests) {
-    this.name = {
-      firstName,
-      lastName
-    };
-    this.age = age;
-    this.gender = gender;
-    this.personalInterests = personalInterests;
-    this.bio = function() {
-      console.log(this.name.firstName + ' ' + this.name.lastName + ' is ' + this.age + ' years old. He likes ' + this.personalInterests[0] + ' and ' + this.personalInterests[1] + '.');
-    };
-    this.greeting = function() {
-      console.log('Hi there! I\'m ' + this.name.firstName + '.');
-    };
-  }
+class Vehicle {
+  constructor(numberOfWheels, brand, milesCovered, model, yearManufacutured, soldOn) {
 
-  const newPerson = new PersonalData('Samantha', 'Briggs', 32, 'Female', ['Swimming', 'Reading', 'Singing']);
+    let _numberOfWheels = numberOfWheels;
+    let _brand = brand;
+    let _milesCovered = milesCovered;
+    let _model = model;
+    let _yearManufactured = yearManufacutured;
+    let _soldOn = soldOn;
+
+    this.salePrice = () => {
+      if (_soldOn !== null) {
+        return 0;
+      }
+      else {
+        return 70000 * _numberOfWheels;
+      }
+    }
+
+    this.purchasePrice = () => {
+      if (_soldOn !== null) {
+        return 0;
+      }
+      else {
+        return _baseSaleCost - _milesCovered;
+      }
+    }
+
+    class Car extends Vehicle {
+      constructor(numberOfWheels, brand, milesCovered, model, yearManufacutured, soldOn) {
+
+        let _numberOfWheels = numberOfWheels;
+        let _brand = brand;
+        let _milesCovered = milesCovered;
+        let _model = model;
+        let _yearManufactured = yearManufacutured;
+        let _soldOn = soldOn;
+        let _baseSaleCost = 1000000;
+         }
+    }
+
+    class Truck extends Vehicle {
+      constructor(numberOfWheels, brand, milesCovered, model, yearManufacutured, soldOn) {
+
+        let _numberOfWheels = numberOfWheels;
+        let _brand = brand;
+        let _milesCovered = milesCovered;
+        let _model = model;
+        let _yearManufactured = yearManufacutured;
+        let _soldOn = soldOn;
+        let _baseSaleCost = 5000000;
+      }
+    }
+    
+  }
+}
